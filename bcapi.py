84a6c00eaa674fce46b2bcb68bf0c6b0a9279e9d
mkaya13 <53873573+mkaya13@users.noreply.github.com>
2022-06-23 15:40:09 +0300

    Update bcapi.py

--- bcapi.py
+++ bcapi.py
@@ -93,7 +93,7 @@ def get_all_petros_holders():
 
 @app.get("/all-petros-holder-count")
 
-def get_all_petros_holders():
+def get_all_petros_holders_count():
     
     infura_url = "https://rinkeby.infura.io/v3/1ca5ad9c22fc4bc3b088ee54d9af02a4"  # Rinkeby BC
     web3 = Web3(Web3.HTTPProvider(infura_url))
