593f9ea182b79e37fda1c2f19f17e2ba1d0fbe5d
mkaya13 <53873573+mkaya13@users.noreply.github.com>
2022-06-23 14:00:55 +0300

    Update bcapi.py

--- bcapi.py
+++ bcapi.py
@@ -66,28 +66,12 @@ def get_all_petros_holders():
     
     scraper = cloudscraper.create_scraper(browser={'platform': 'windows'})
 
-    headers = {
-    'authority': 'rinkeby.etherscan.io',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'accept-language': 'en-US,en;q=0.9',
-    'cache-control': 'max-age=0',
-    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'none',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36',
-    }
-
     params = {
     'a': '0xD8AFa55703A442a127761E5CA897e060Cb3dcb2b',
     'p': '1',
     }
 
-    response = scraper.get('https://rinkeby.etherscan.io/token/generic-tokenholders2', params=params, headers=headers)
+    response = scraper.get('https://rinkeby.etherscan.io/token/generic-tokenholders2', params=params)
     
     print(response.status_code)
 
@@ -125,28 +109,12 @@ def get_all_petros_holders():
     
     scraper = cloudscraper.create_scraper(browser={'platform': 'windows'})
 
-    headers = {
-    'authority': 'rinkeby.etherscan.io',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'accept-language': 'en-US,en;q=0.9',
-    'cache-control': 'max-age=0',
-    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'none',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36',
-    }
-
     params = {
     'a': '0xD8AFa55703A442a127761E5CA897e060Cb3dcb2b',
     'p': '1',
     }
 
-    response = scraper.get('https://rinkeby.etherscan.io/token/generic-tokenholders2', params=params, headers=headers)
+    response = scraper.get('https://rinkeby.etherscan.io/token/generic-tokenholders2', params=params)
 
     total_token_holder_count = int(re.findall(r"nA total of(.*?) token holders\\", str(response.content))[0].strip(' '))
 
@@ -161,28 +129,12 @@ def all_petros_sorted_ETH():
     
     scraper = cloudscraper.create_scraper(browser={'platform': 'windows'})
 
-    headers = {
-        'authority': 'rinkeby.etherscan.io',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'accept-language': 'en-US,en;q=0.9',
-        'cache-control': 'max-age=0',
-        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'document',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-site': 'none',
-        'sec-fetch-user': '?1',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36',
-    }
-
     params = {
         'a': '0xD8AFa55703A442a127761E5CA897e060Cb3dcb2b',
         'p': '1',
     }
 
-    response = scraper.get('https://rinkeby.etherscan.io/token/generic-tokenholders2', params=params, headers=headers)
+    response = scraper.get('https://rinkeby.etherscan.io/token/generic-tokenholders2', params=params)
 
 
     total_token_holder_count = int(re.findall(r"nA total of(.*?) token holders\\", str(response.content))[0].strip(' '))
