1082ffb4a1a101386c8dab1743ff1c94a7963772
mkaya13 <53873573+mkaya13@users.noreply.github.com>
2022-06-26 20:07:23 +0300

    Update bcapi.py

--- bcapi.py
+++ bcapi.py
@@ -20,8 +20,18 @@ import json,requests
 from web3 import Web3
 import re
 import cloudscraper
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/all-SFT-metadata")
 
